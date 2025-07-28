3a38a82c4bbf4d8c556dc1473225de39a92ca412
judy99 <julia.kurianova@gmail.com>
2025-07-28 18:06:26 -0400

    feat: add handler to update todoList

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,11 +1,23 @@
-const TodoForm = () => {
+import { useRef } from 'react';
+
+const TodoForm = ({ onAddTodo }) => {
+  const todoTitleInput = useRef('')
+  
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    const title = event.target.title.value;
+    onAddTodo(title);
+    event.target.title.value = '';
+    todoTitleInput.current.focus();
+  };
+
   return (
-    <form>
+    <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Todo</label>
-      <input type="text" id="todoTitle" />
+      <input type="text" id="todoTitle" name="title" ref={todoTitleInput}/>
       <button>Add Todo</button>
     </form>
   );
-}
+};
 
 export default TodoForm;
